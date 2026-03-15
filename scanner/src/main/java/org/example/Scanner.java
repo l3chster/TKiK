87b5e58550ff87815f9ae2b88a8b0ca123ce591c
unknown <Lechster1.0@gmail.com>
2026-03-15 11:00:30 +0100

    skipping whitespaces between tokens

--- scanner/src/main/java/org/example/Scanner.java
+++ scanner/src/main/java/org/example/Scanner.java
@@ -22,7 +22,13 @@ public class Scanner {
         {                                              // EDIT:: w javie nie ma EOF chb musi tak zostac
             char sign = this.text.charAt(this.iterator);
 
-            if (Character.isDigit(sign))
+            if(Character.isWhitespace(sign))    // pomijanie białych znaków miedzy tokenami
+            {
+                iterator++;
+                continue;
+            }
+
+            if (Character.isDigit(sign)) // zrobic pominiecie bialych znakow i spacji szzegolnie czyli troche zmodyfikowac
             {
                 token_value += sign;
                 this.iterator++;
@@ -41,6 +47,11 @@ public class Scanner {
                 magazine.add_token(num_token);
                 token_value = "";
             }
+
+            // tu kolejne ify
+            // dla zmiennych bd trzeba wziac pod uwage ze moze byc np x33 i wtedy bierzemy caly token aż do znaku specjalnego
+
+
 
             this.iterator++;
         }
